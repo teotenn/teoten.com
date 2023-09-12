85782d2efae9aa0f11918235eb722513cd89dcc3
Manuel Teodoro <teoten@gmail.com>
2023-09-12 12:48:39 +0200

    Personalization completed; Contact form removed

--- assets/form.php
+++ assets/form.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'info@teoten.com';
+$from = 'teotenn@proton.me';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'teotenn@proton.me';
+$sendTo = 'teoten@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
